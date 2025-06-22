d5e6ed65279cd52a60e8ab4eecbb46d36f091d52
Daria Krupinska <dkrupinska@gmail.com>
2025-06-22 13:38:52 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+counter = {"value": 0}
+
+@app.get("/count")
+def get_count():
+    return {"count": counter["value"]}
+
+@app.post("/click")
+def register_click():
+    counter["value"] += 1
+    return {"count": counter["value"]}
